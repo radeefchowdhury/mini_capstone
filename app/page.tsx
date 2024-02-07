b65299a666e3e21662e18ac17e297af80c17644c
ismail <ismail.feham64@gmail.com>
2024-02-07 00:28:56 -0500

    file clean up

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,8 @@
-import Image from "next/image";
-import Link from "next/link";
-import SignUp from "./components/SigningUp/SigningUp/SignUp"; 
-import bar from "./components/Navigation/bar";
 
 export default function Home() {
   return (
     <main>
-      {/* <Link href={"/users"}>
-        Users
-      </Link> */}
-      <SignUp /> 
-      <bar />
+      hello
     </main>
   );
 }
