cbcf5c732ee63541e5611bcc98bc87704f6bf210
Ismail Feham <57361300+FehamIsmail@users.noreply.github.com>
2024-03-22 21:36:05 -0400

    Update Login.tsx
    typo

--- src/app/components/Authentification/Login.tsx
+++ src/app/components/Authentification/Login.tsx
@@ -56,7 +56,7 @@ const Login = () => {
 
                     })
                 }
-                // window.location.href = '/'
+                window.location.href = '/'
             })
             .catch(console.error)
     }
