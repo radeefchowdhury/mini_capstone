4ea3ab745abd7785cd8ba4dd725616662024eb7b
ismail <ismail.feham64@gmail.com>
2024-03-24 07:42:29 -0400

    properties refactoring

--- src/app/dashboard/properties/PropertyFilesView.tsx
+++ src/app/dashboard/properties/PropertyFilesView.tsx
@@ -1,35 +1,40 @@
 import React, {useEffect} from 'react';
-import {CondoFileType} from "@/app/constants/types";
+import {CondoFileType, PropertyType} from "@/app/constants/types";
 import {
-    createCondoFileInstance,
-    getCondoFileURL,
     getCondoIDFromName,
-    getFilesFromProperty,
-    uploadCondoFile
 } from "@/app/api/property/PropertyAPI";
 import PopupPanel from "@/app/components/dashboard/PopupPanel";
 import DashboardTable from "@/app/components/dashboard/DashboardTable";
 import ActionIcon from "@/app/components/dashboard/ActionIcon";
-import {ArrowDownTrayIcon, ArrowLeftIcon, PencilSquareIcon} from "@heroicons/react/24/outline";
+import {ArrowLeftIcon, ArrowUpRightIcon, PencilSquareIcon} from "@heroicons/react/24/outline";
+import {
+    deleteCondoFile,
+    getCondoFileURL,
+    getFilesFromProperty,
+    submitCondoFile,
+    uploadCondoFile
+} from "@/app/api/property/CondoFileAPI";
 
 interface PropertyFilesViewProps {
-    viewFiles: boolean;
-    setViewFiles: (value: boolean) => void;
-    propertyId: number;
+    isVisible: boolean;
+    setIsVisible: (value: boolean) => void;
+    property: PropertyType;
 }
 function PropertyFilesView(props: PropertyFilesViewProps) {
-    const {viewFiles, setViewFiles, propertyId} = props;
+    const {isVisible, setIsVisible, property} = props;
     const [files, setFiles] = React.useState<CondoFileType[]>([]);
     const [formAction, setFormAction] = React.useState<'UPLOAD' | 'VIEW' | 'EDIT'>('VIEW');
     const [selectedFile, setSelectedFile] = React.useState<CondoFileType>();
     const [filteredFiles, setFilteredFiles] = React.useState<any[]>([]);
     const [newFileInfo, setNewFileInfo] = React.useState<CondoFileType>({} as CondoFileType)
     const [file, setFile] = React.useState<File|null>(null);
-
+    const [filesToDelete, setFilesToDelete] = React.useState<number[]>([]);
 
     const selectFile = (file: CondoFileType) => {
         setSelectedFile(file)
         setFormAction('EDIT')
+        setFile(null)
+        setFilesToDelete([])
     }
 
     const handleFileChange = (e: any) => {
@@ -39,62 +44,118 @@ function PropertyFilesView(props: PropertyFilesViewProps) {
 
     const uploadNewFile = () => {
         if(!file) return
-        let unit_id = -1;
         let file_src = "";
         uploadCondoFile(file, newFileInfo.name).catch(console.error).then(() => {
             getCondoFileURL(newFileInfo.name).catch(console.error).then((data) => {
                 if(data) file_src = data
             }).then(() => {
-                getCondoIDFromName(newFileInfo.unit.name).catch(console.error).then(res => {
-                    if(res && res.data) unit_id = res.data[0].id
-                }).then(() => {
-                    if(unit_id === -1) return
-                    if(file_src === "") return
-                    createCondoFileInstance({
-                        name: newFileInfo.name,
-                        type: newFileInfo.type,
-                        src: file_src,
-                        unit_id: unit_id
-                    }).then(()=> {
-                        window.location.reload()
-                    })
-                })
+                upsertCondoFile(newFileInfo, file_src).then(r => console.log(r))
             })
         }).catch(console.error)
+    }
 
+    const toggleFileToDelete = (id: number) => {
+        setFilesToDelete(prevFilesToDelete => {
+            if (prevFilesToDelete.includes(id)) {
+                // If the file is already selected, remove it from the list
+                return prevFilesToDelete.filter(file_id => file_id !== id);
+            } else {
+                // If the file is not already selected, add it to the list
+                return [...prevFilesToDelete, id];
+            }
+        });
+    }
+
+    const deleteSelectedCondoFile = (id: number) => {
+        deleteCondoFile(id).catch(console.error).then(() => {
+            window.location.reload()
+        })
     }
 
     const downloadFile = (fileSrc: string) => {
-        console.log(fileSrc)
-        // window.open(fileSrc)
+        window.open(fileSrc)
+    }
+
+    const saveFileOnClick = async () => {
+        if(!selectedFile) return
+        let file_src = "";
+        if(file){
+            uploadCondoFile(file, selectedFile.name).catch(console.error).then(() => {
+                getCondoFileURL(selectedFile.name).catch(console.error).then((data) => {
+                    if(data) file_src = data
+                    console.log(file_src)
+                }).then(() => {
+                    upsertCondoFile(selectedFile, file_src)
+                })
+            }).catch(console.error)
+        }
+        else await upsertCondoFile(selectedFile, selectedFile.src)
+
+    }
+
+    const upsertCondoFile = async (file_to_upsert: CondoFileType, fileSrc: string) => {
+        let unit_id = -1;
+        let file_src = fileSrc;
+        await getCondoIDFromName(file_to_upsert.unit.name).catch(console.error).then(res => {
+            if(res && res.data) unit_id = res.data[0].id
+        }).then(() => {
+            if(unit_id === -1 || fileSrc === "") return
+            submitCondoFile({
+                id: file_to_upsert.id,
+                name: file_to_upsert.name,
+                type: file_to_upsert.type,
+                src: file_src,
+                unit_id: unit_id
+            }).then(()=> {
+                window.location.reload()
+            })
+        })
+    }
+
+    const fetchData = async () => {
+        if (!property.id) return;
+        const {data, error} = await getFilesFromProperty(property.id);
+        if(error){
+            console.log(error)
+            return
+        }
+        if(data) setFiles(data)
     }
 
     useEffect(() => {
-        if (propertyId === -1) return;
-        const fetchData = async () => {
-            const {data, error} = await getFilesFromProperty(propertyId);
-            if(error){
-                console.log(error)
-                return
-            }
-            if(data) setFiles(data)
-            console.log(data)
+        if(!isVisible){
+            setFiles([])
+            setFilesToDelete([])
+            setSelectedFile(undefined)
+            setFile(null)
+            setNewFileInfo({} as CondoFileType)
         }
-        fetchData().catch(console.error)
-    }, [propertyId]);
+        else fetchData().catch(console.error)
+        if(!isVisible) setFormAction('VIEW')
+    }, [isVisible]);
 
     useEffect(() => {
-        if(files.length == 0) return;
+        fetchData().catch(console.error)
+    }, [property]);
+
+    useEffect(() => {
+        if(files.length == 0){
+            setFilteredFiles([])
+            return
+        }
         let filteredData = files.map((condoFile) => {
             return {
                 id: condoFile.id,
-                name: condoFile.name,
+                name:
+                    <div className={"flex flex-row gap-4 px-2"}><input type="checkbox" onChange={() => toggleFileToDelete(condoFile.id)}/>
+                    {condoFile.name}
+                    </div>,
                 type: condoFile.type,
-                condo_name: condoFile.unit.name,
+                condo_name: condoFile.unit?.name,
                 src: condoFile.src,
                 edit:   <div className={"flex gap-1 flex-row"}>
                             <ActionIcon Icon={PencilSquareIcon} onClick={() => selectFile(condoFile)}/>
-                            <ActionIcon Icon={ArrowDownTrayIcon} onClick={() => downloadFile(condoFile.src)}/>
+                            <ActionIcon Icon={ArrowUpRightIcon} onClick={() => downloadFile(condoFile.src)}/>
                         </div>
             }
         })
@@ -105,23 +166,36 @@ function PropertyFilesView(props: PropertyFilesViewProps) {
         <>
             {formAction === "VIEW" &&
                 <PopupPanel
-                title={'Property Files'}
+                title={'Property Files: ' + property.name || ' '}
                 buttonTitle={"Upload new File"}
                 children={
                     <div>
-                        <DashboardTable
-                        headers={[
-                            {name: 'File Name', key: 'name'},
-                            {name: 'File Type', key: 'type'},
-                            {name: 'Condo Name', key: 'condo_name'},
-                            {name: 'Actions', key: 'edit'},
-                        ]}
-                        items={filteredFiles}/>
+                        <div>
+                            <DashboardTable
+                                headers={[
+                                    {name: 'File Name', key: 'name'},
+                                    {name: 'File Type', key: 'type'},
+                                    {name: 'Condo Name', key: 'condo_name'},
+                                    {name: 'Actions', key: 'edit'},
+                                ]}
+                                items={filteredFiles}/>
+                        </div>
+                        <button
+                            // style it so that it is a light indigo when there are no files to delete selected
+                            // and a normal indigo when there are files to delete selected
+                            className={`w-fit px-2 py-1 mt-4 rounded-md ${filesToDelete.length === 0 ? "bg-indigo-200 cursor-default" : "bg-indigo-500 hover:bg-indigo-700"} text-white`}
+                            disabled={filesToDelete.length === 0}
+                            onClick={() => {
+                            filesToDelete.forEach((file_id) => {
+                                deleteSelectedCondoFile(file_id)
+                            })
+                        }}>Delete Selected Files
+                        </button>
                     </div>
                 }
                 onClick={() => setFormAction('UPLOAD')}
-                visible={viewFiles}
-                setVisible={setViewFiles}/>
+                visible={isVisible}
+                setVisible={setIsVisible}/>
             }
             {formAction === "UPLOAD" &&
                 <PopupPanel
@@ -172,9 +246,9 @@ function PropertyFilesView(props: PropertyFilesViewProps) {
                             </div>
                         </>
                     }
-                    visible={viewFiles}
+                    visible={isVisible}
                     onClick={uploadNewFile}
-                    setVisible={setViewFiles}/>
+                    setVisible={setIsVisible}/>
             }
             {formAction === "EDIT" &&
                 <PopupPanel
@@ -186,11 +260,48 @@ function PropertyFilesView(props: PropertyFilesViewProps) {
                                 className={"mt-3 mb-6 w-6 h-6 text-slate-700 cursor-pointer"}
                                 onClick={() => setFormAction('VIEW')}
                             />
-                            Edit File
+                            {selectedFile &&
+                                <div className={"text-slate-500 font-medium flex flex-col gap-3"}>
+                                <div className="flex flex-col gap-1">
+                                    <label htmlFor="File Name" className="">File Name</label>
+                                    <input
+                                        value={selectedFile.name || ""}
+                                        onChange={(e) => setSelectedFile({...selectedFile, name: e.target.value})}
+                                        type="text" id="name" name="name"
+                                        className="p-2 w-full border border-slate-300 focus:outline-slate-500 rounded-md"/>
+                                </div>
+                                <div className="flex flex-col gap-1">
+                                    <label htmlFor="File Type" className="">File Type</label>
+                                    <input
+                                        value={selectedFile.type || ""}
+                                        onChange={(e) => setSelectedFile({...selectedFile, type: e.target.value})}
+                                        type="text" id="type" name="type"
+                                        className="p-2 w-full border border-slate-300 focus:outline-slate-500 rounded-md"/>
+                                </div>
+                                <div className="flex flex-col gap-1">
+                                    <label htmlFor="Condo name" className="">Condo Name</label>
+                                    <input
+                                        pattern={"[A-Za-z]*"}
+                                        value={selectedFile.unit?.name || ""}
+                                        onChange={(e) => setSelectedFile({...selectedFile, unit: {id: 0, name: e.target.value}})}
+                                        type="text" id="type" name="type"
+                                        className="p-2 w-full border border-slate-300 focus:outline-slate-500 rounded-md"/>
+                                </div>
+                                <div className="flex flex-col gap-1">
+                                    <label htmlFor="Property Address" className="">File Upload</label>
+                                    <input
+                                        className={`mb-3 relative m-0 block w-full min-w-0 flex-auto cursor-pointer rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-sm font-normal text-neutral-700 transition duration-200 ease-in-out file:-mx-3 file:-my-[0.32rem] file:cursor-pointer file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none `}
+                                        onChange={handleFileChange}
+                                        type="file" data-testid="profile-picture-upload" id={"profile_picture"}
+                                        name={"profile_picture"}
+                                    />
+                                </div>
+                            </div>}
                         </>
                     }
-                    visible={viewFiles}
-                    setVisible={setViewFiles}/>
+                    visible={isVisible}
+                    onClick={saveFileOnClick}
+                    setVisible={setIsVisible}/>
             }
         </>
     );
