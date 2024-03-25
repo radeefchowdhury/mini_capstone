f4d9788829e8e4f19560ea23eda8293b9a264b50
ismail <ismail.feham64@gmail.com>
2024-03-25 08:34:40 -0400

    updated units/property page to handle view locker/parking

--- src/app/dashboard/properties/page.tsx
+++ src/app/dashboard/properties/page.tsx
@@ -11,13 +11,14 @@ import PopupPanel from "@/app/components/dashboard/PopupPanel";
 import ActionIcon from "@/app/components/dashboard/ActionIcon";
 import {PencilSquareIcon} from "@heroicons/react/24/outline";
 import PropertyFilesView from "@/app/dashboard/properties/PropertyFilesView";
+import ParkingLockerView from "@/app/components/dashboard/ParkingLockerView";
 
 const propertyTableHeaders = [
     {name: 'Property Name', key: 'name'},
     {name: 'Address', key: 'address'},
     {name: 'Unit Count', key: 'unit_count'},
-    {name: 'Parking Count', key: 'parking_count'},
-    {name: 'Locker Count', key: 'locker_count'},
+    {name: 'Parking Spots', key: 'parking_count'},
+    {name: 'Lockers', key: 'locker_count'},
     {name: 'Condo files', key: 'condo_files'},
     {name: 'Actions', key: 'edit'},
 ]
@@ -31,8 +32,13 @@ function Page() {
     const [selectedProperty, setSelectedProperty] = React.useState<PropertyType>();
     const [newPropertyProfile, setNewPropertyProfile] = React.useState<PropertyType>({} as PropertyType);
     const [formAction, setFormAction] = React.useState<'EDIT' | 'CREATE'>();
+
     const [viewFiles, setViewFiles] = React.useState<boolean>(false);
-    const [selectedPropertyFiles, setSelectedPropertyFiles] = React.useState<PropertyType>({} as PropertyType);
+    const [selectedPropertyView, setSelectedPropertyView] = React.useState<PropertyType>({} as PropertyType);
+
+    const [viewParkingLocker, setViewParkingLocker] = React.useState<boolean>(false);
+    const [parkingLockerView, setParkingLockerView] = React.useState<'PARKING' | 'LOCKER'>('PARKING');
+    const [parkingLockerList, setParkingLockerList] = React.useState<any[]>([]);
 
 
     const selectProperty = (property: PropertyType) => {
@@ -44,12 +50,19 @@ function Page() {
     }
 
     const viewPropertyFiles = (property: PropertyType) => {
-        setSelectedPropertyFiles(property)
+        setSelectedPropertyView(property)
         setViewFiles(true)
     }
 
     const submitNewProperty = () => {
         submitPropertyProfile(newPropertyProfile).catch(console.error)
+    }
+
+    const viewParkingLockerOnClick = (property: PropertyType, type: 'PARKING' | 'LOCKER') => {
+        setSelectedPropertyView(property)
+        setViewParkingLocker(true)
+        setParkingLockerView(type)
+        setParkingLockerList(type == 'PARKING' ? property.parking_spots || [] : property.lockers || [])
     }
 
     const fetchPropertyData = async () => {
@@ -72,9 +85,6 @@ function Page() {
     useEffect(() => {
         if(propertyData.length == 0) return;
         let filteredData = propertyData.map((property) => {
-            // property has a list of units, each unit has a list of files
-            // I need to get the count of all files in all condos
-            // make sure that it is TS compliant
             const fileCount = property.units?.reduce((acc: number, unit) => {
                 return acc + (unit.files ? unit.files.length : 0);
             }, 0) || 0;
@@ -82,9 +92,9 @@ function Page() {
                 id: property.id,
                 name: property.name,
                 address: property.address,
-                unit_count: property.unit_count,
-                parking_count: property.parking_count,
-                locker_count: property.locker_count,
+                unit_count:  <span>{property.units?.length || 0}</span>,
+                parking_count: <ActionButton title={"View Parking (" + property.parking_spots?.length +")"} onClick={() => viewParkingLockerOnClick(property, 'PARKING')}/>,
+                locker_count: <ActionButton title={"View Lockers (" + property.lockers?.length +")"} onClick={() => viewParkingLockerOnClick(property, 'LOCKER')}/>,
                 condo_files: <ActionButton title={"View Files (" + fileCount + ")"} onClick={() => viewPropertyFiles(property)}/>,
                 edit: <ActionIcon Icon={PencilSquareIcon} onClick={() => selectProperty(property)}/>
             }
@@ -99,7 +109,20 @@ function Page() {
 
     return (
         <div className={"flex flex-col xl:flex-row sm:gap-[36px] gap-[28px]"}>
-            <PropertyFilesView isVisible={viewFiles} setIsVisible={setViewFiles} property={selectedPropertyFiles}/>
+            <PropertyFilesView
+                isVisible={viewFiles}
+                setIsVisible={setViewFiles}
+                property={selectedPropertyView}
+            />
+            <ParkingLockerView
+                type={parkingLockerView}
+                view={userType == 'COMPANY' ? 'COMPANY' : 'PUBLIC'}
+                from={'PROPERTY'}
+                isVisible={viewParkingLocker}
+                selectedItem={selectedPropertyView}
+                items={parkingLockerList}
+                setIsVisible={setViewParkingLocker}
+            />
             <div className={"min-w-0 max-w-fit"}>
                 <DashboardPanel
                     title={'My Properties'}
