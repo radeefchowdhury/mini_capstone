980d8c27449622bf30996399e43864d9e9c2e121
ismail <ismail.feham64@gmail.com>
2024-04-13 20:02:21 -0400

    changed nextConfig to use .env attributes

--- src/next.config.mjs
+++ src/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        NEXT_PUBLIC_SUPABASE_URL: 'https://skajanyaifqitnhraxod.supabase.co',
-        NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNrYWphbnlhaWZxaXRuaHJheG9kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcyODQyMjcsImV4cCI6MjAyMjg2MDIyN30.71vRsoOvwHyhef_iEcwWK50FGb-TBHPhoMf3bYrOTeE',
+        NEXT_PUBLIC_SUPABASE_URL: process.env.PUBLIC_SUPABASE_URL,
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.PUBLIC_SUPABASE_ANON_KEY,
         },
     images: {
         remotePatterns: [
